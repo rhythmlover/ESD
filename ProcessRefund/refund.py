ba53c0d7ff67f422f951ac7e00cda3bf43a58978
dominicteh1 <dominicteh.2022@scis.smu.edu.sg>
2024-03-10 18:54:45 +0800

    implement firestore for refund.py

--- ProcessRefund/refund.py
+++ ProcessRefund/refund.py
@@ -1,121 +1,110 @@
-#!/usr/bin/env python3
-# The above shebang (#!) operator tells Unix-like environments
-# to run this file as a python3 script
-
-import os
 from flask import Flask, request, jsonify
-from flask_sqlalchemy import SQLAlchemy
-
+import firebase_admin
+from firebase_admin import firestore, credentials
 from datetime import datetime
 
+# Intialization of Flask app and Firebase Firestore
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:root@localhost:3306/refund'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 299}
+cred = credentials.Certificate("../esd-ticketing-firebase-adminsdk-dxgtc-363d36e381.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
-db = SQLAlchemy(app)
+# Global variables
+now = datetime.now()
+formatted_time = now.strftime("%Y-%m-%dT%H:%M:%SZ")
 
+# Endpoints
+@app.route("/refunds/<string:event_id>")
+def get_event_refunds(event_id):
+    """
+    Retrieves all refunds for a specific event.
+    ---
+    parameters:
+        -   in: path
+            name: event_id
+            required: true
+    responses:
+        200:
+            description: Return the refunds for the event with the specified event_id
+        404:
+            description: No refunds for this event found.
+    """
+    refunds_ref = db.collection("reviews").document(event_id).collection("event_refunds")
+    docs = refunds_ref.stream()
 
-class Refund(db.Model):
-    __tablename__ = 'refund'
+    refunds = []
+    for doc in docs:
+        refunds.append(doc.to_dict())
 
-    refund_id = db.Column(db.Integer, primary_key=True)
-    customer_id = db.Column(db.String(32), nullable=False)
-    status = db.Column(db.String(10), nullable=False)
-    created = db.Column(db.DateTime, nullable=False, default=datetime.now)
-    modified = db.Column(db.DateTime, nullable=False,
-                         default=datetime.now, onupdate=datetime.now)
-
-    def json(self):
-        dto = {
-            'refund_id': self.refund_id,
-            'customer_id': self.customer_id,
-            'status': self.status,
-            'created': self.created,
-            'modified': self.modified
-        }
-
-        dto['refund_item'] = []
-        for oi in self.refund_item:
-            dto['refund_item'].append(oi.json())
-
-        return dto
-
-
-class Refund_Item(db.Model):
-    __tablename__ = 'refund_item'
-
-    item_id = db.Column(db.Integer, primary_key=True)
-    refund_id = db.Column(db.ForeignKey(
-        'refund.refund_id', ondelete='CASCADE', onupdate='CASCADE'), nullable=False, index=True)
-
-    book_id = db.Column(db.String(13), nullable=False)
-    quantity = db.Column(db.Integer, nullable=False)
-
-    # refund_id = db.Column(db.String(36), db.ForeignKey('refund.refund_id'), nullable=False)
-    # refund = db.relationship('Refund', backref='refund_item')
-    refund = db.relationship(
-        'Refund', primaryjoin='Refund_Item.refund_id == Refund.refund_id', backref='refund_item')
-
-    def json(self):
-        return {'item_id': self.item_id, 'book_id': self.book_id, 'quantity': self.quantity, 'refund_id': self.refund_id}
-
-
-@app.route("/refund")
-def get_all():
-    refundlist = db.session.scalars(db.select(Refund)).all()
-    if len(refundlist):
+    if refunds:
         return jsonify(
             {
                 "code": 200,
-                "data": {
-                    "refunds": [refund.json() for refund in refundlist]
-                }
+                "data": refunds
             }
         )
     return jsonify(
         {
             "code": 404,
-            "message": "There are no refunds."
+            "message": "No refunds for this event found."
         }
     ), 404
 
-
-@app.route("/refund/<string:refund_id>")
-def find_by_refund_id(refund_id):
-    refund = db.session.scalars(
-        db.select(Refund).filter_by(refund_id=refund_id).limit(1)).first()
-    if refund:
-        return jsonify(
-            {
-                "code": 200,
-                "data": refund.json()
-            }
-        )
-    return jsonify(
-        {
-            "code": 404,
-            "data": {
-                "refund_id": refund_id
-            },
-            "message": "Refund not found."
-        }
-    ), 404
-
-
-@app.route("/refund", methods=['POST'])
-def create_refund():
-    customer_id = request.json.get('customer_id', None)
-    refund = Refund(customer_id=customer_id, status='NEW')
-
-    cart_item = request.json.get('cart_item')
-    for item in cart_item:
-        refund.refund_item.append(Refund_Item(
-            book_id=item['book_id'], quantity=item['quantity']))
-
+@app.route("/refunds/<string:event_id>", methods=['POST'])
+def create_refund(event_id):
+    """
+    Create a refund request for a specific event.
+    ---
+    parameters:
+        -   in: path
+            name: event_id
+            required: true
+    requestBody:
+        description: Refund details
+        required: true
+        content:
+            application/json:
+                schema:
+                    properties:
+                        user_id:
+                            type: string
+                            description: ID of user who requested the refund
+                        ticket_id: 
+                            type: number
+                            description: ID of the ticket to be refunded
+    responses:
+        201:
+            description: Refund request created successfully
+        400:
+            description: Missing required fields in body
+        500:
+            description: Internal server error
+    """
     try:
-        db.session.add(refund)
-        db.session.commit()
+        required_fields = ['user_id', 'ticket_id']
+        if not all(field in request.json for field in required_fields):
+            return jsonify(
+                {
+                    "code": 400,
+                    "message": "Missing required fields. Please provide user_id and ticket_id."
+                }
+            ), 400
+
+        doc_ref = db.collection("refunds").document(
+            event_id).collection("event_refunds").document()
+        doc_ref.set({
+            'user_id': request.json['user_id'],
+            'created_at': formatted_time,
+            'event_id': event_id,
+            'ticket_id': request.json['ticket_id']
+        })
+
+        return jsonify(
+            {
+                "code": 201,
+                "message": "Refund request created successfully."
+            }
+        ), 201
     except Exception as e:
         return jsonify(
             {
@@ -124,54 +113,5 @@ def create_refund():
             }
         ), 500
 
-    return jsonify(
-        {
-            "code": 201,
-            "data": refund.json()
-        }
-    ), 201
-
-
-@app.route("/refund/<string:refund_id>", methods=['PUT'])
-def update_refund(refund_id):
-    try:
-        refund = db.session.scalars(
-        db.select(Refund).filter_by(refund_id=refund_id).
-        limit(1)).first()
-        if not refund:
-            return jsonify(
-                {
-                    "code": 404,
-                    "data": {
-                        "refund_id": refund_id
-                    },
-                    "message": "Refund not found."
-                }
-            ), 404
-
-        # update status
-        data = request.get_json()
-        if data['status']:
-            refund.status = data['status']
-            db.session.commit()
-            return jsonify(
-                {
-                    "code": 200,
-                    "data": refund.json()
-                }
-            ), 200
-    except Exception as e:
-        return jsonify(
-            {
-                "code": 500,
-                "data": {
-                    "refund_id": refund_id
-                },
-                "message": "An error occurred while updating the refund. " + str(e)
-            }
-        ), 500
-
-
 if __name__ == '__main__':
-    print("This is flask for " + os.path.basename(__file__) + ": manage refunds ...")
     app.run(host='0.0.0.0', port=5002, debug=True)
